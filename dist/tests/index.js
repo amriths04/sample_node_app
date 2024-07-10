894c35d76dc819f012f90d4a0a5681cb331cf2a4
amrith <amrithshet25@gmail.com>
2024-07-10 19:18:01 +0530

    removed dist

--- dist/tests/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const HelloWorld_1 = require("../src/HelloWorld");
-console.log("hello world");
-const Hw = new HelloWorld_1.HelloWorld();
-Hw.Hello();
